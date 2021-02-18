44784c4ebe1be111bfe698df2b43abc5dc7dca4c
Sim kaur <moblue2012@gmail.com>
2021-02-18 17:42:17 -0500

    added row for map and listing content

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -15,13 +15,25 @@ const Landing = props => {
       <div className="row">
         <div className="col s12 center-align">
           <h4>
-            <b>YSale</b> A new and better way to yard sale! {' '} <span style={{ fontFamily: 'monospace' }}>Yard Sale</span> 
+            <b>YSale</b> A new and better way to yard sale! {' '} <span style={{ fontFamily: 'monospace' }}>Yard Sale</span>
           </h4>
           <br />
-          
+            <div className="col s6">
+              <card className="salesMap">
+                <p> this is where the map will go</p>
+              </card>
+            </div>
+
+            <div className="col s6">
+              <card className="salesList">
+                <p>this is where the listed sales will go</p>
+              </card>
+            </div>
+
+
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
