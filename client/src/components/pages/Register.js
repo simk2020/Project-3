68e2a3bb91351a457e5c8d82c6f8f907266b9df4
Sarah Carlson <sbcarls@gmail.com>
2021-03-05 17:06:51 -0500

    styling for register page

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -35,15 +35,15 @@ const Register = props => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col s8 offset-s2">
-          <Link to="/" className="btn-flat waves-effect">
+        <div className="card-panel col s8 offset-s2">
+          {/* <Link to="/" className="btn-flat waves-effect">
             <i className="material-icons left">keyboard_backspace</i> Back to home
-          </Link>
-          <div className="col s12" style={{ paddingLeft: '11.250px' }}>
+          </Link> */}
+          <div className="col s12 center-align" style={{ paddingLeft: '11.250px' }}>
             <h4>
               <b>Register</b> below
             </h4>
-            <p className="grey-text text-darken-1">
+            <p className="black-text text-darken-1">
               Already have an account? <Link to="/login">Login</Link>
             </p>
           </div>
@@ -72,8 +72,8 @@ const Register = props => {
               <label htmlFor="password2">Confirm Password</label>
               <span className="red-text">{errors.password2}</span>
             </div>
-            <div className="col s12" style={{ paddingLeft: '11.250px' }}>
-              <button className="btn btn-large waves-effect waves-light hoverable blue accent-3" style={{
+            <div className="col s12 center-align" style={{ paddingLeft: '11.250px' }}>
+              <button className="btn btn waves-effect waves-light" style={{
                 width: '150px',
                 borderRadius: '3px',
                 letterSpacing: '1.5px',
